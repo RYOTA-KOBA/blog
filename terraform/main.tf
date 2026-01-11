62574599e654d6e13ff9fc7b4ca78e5be1d45209
RYOTA-KOBA <ryotakobayashi04@gmail.com>
2026-01-12 01:42:59 +0900

    一旦pagesを消してみる

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,13 @@
-resource "cloudflare_pages_project" "blog" {
-  account_id        = var.cloudflare_account_id
-  name              = var.project_name
-  production_branch = var.production_branch
+# Pages project managed manually via Cloudflare dashboard
+# (Created and configured outside of Terraform due to authentication constraints)
 
-  source {
-    type = "github"
+# output "cloudflare_pages_project_url" {
+#   description = "Cloudflare Pages Project URL"
+#   value       = "Configure via Cloudflare dashboard"
+# }
 
-    config {
-      owner                      = var.github_owner
-      repo_name                  = var.github_repo
-      production_branch          = var.production_branch
-      pr_comments_enabled        = true
-      deployments_enabled        = true
-      preview_deployment_setting = "all"
-      preview_branch_includes    = ["develop"]
-    }
-  }
+# output "cloudflare_pages_project_name" {
+#   description = "Cloudflare Pages Project Name"
+#   value       = "blog"
+# }
 
-  build_config {
-    build_command   = "npm run build"
-    destination_dir = "out"
-    root_dir        = "public/articles"
-  }
-}
-
-output "cloudflare_pages_project_url" {
-  description = "Cloudflare Pages Project URL"
-  value       = cloudflare_pages_project.blog.domains[0]
-}
-
-output "cloudflare_pages_project_name" {
-  description = "Cloudflare Pages Project Name"
-  value       = cloudflare_pages_project.blog.name
-}
